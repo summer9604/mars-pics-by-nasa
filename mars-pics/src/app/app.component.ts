f6c5a3187f67b77623512b54b43c384519e2a1ab
Ricardo Magalhães <ricardo_energie@hotmail.com>
2020-08-16 00:12:40 +0100

    added pagination

--- mars-pics/src/app/app.component.ts
+++ mars-pics/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mars-pics';
+  title = 'Pictures from Mars by Nasa';
 }
--- mars-pics/src/app/components/mars-pics/mars-pics.component.ts
+++ mars-pics/src/app/components/mars-pics/mars-pics.component.ts
@@ -8,21 +8,78 @@ import { MarsApiService } from 'src/app/services/mars-api.service';
 })
 export class MarsPicsComponent implements OnInit {
 
-  pics : any[] = [];
+  private picsCollection: any[] = [];
+  private picsPerPage: number = 25;
+  private firstPicPerPageIndex : number;
+  private lastPicImageIndex : number;
+  currentPics : any[];
+  selectedPage : HTMLElement;
+  currentPage = 1;
+  numOfPages: number;
 
-  constructor(private marsApiService : MarsApiService) { }
+  constructor(private marsApiService: MarsApiService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { 
     this.getMarsPics();
   }
 
   getMarsPics() {
-    this.marsApiService.getMarsPics()
-        .subscribe(data => this.pics = data.photos);
+    this.marsApiService.getMarsPics().subscribe(data => {   
+      this.picsCollection = data["photos"];
+      this.numOfPages = Math.ceil(this.picsCollection.length / this.picsPerPage);
+      this.firstPicPerPageIndex = 0;
+      this.lastPicImageIndex = 24;
+      this.currentPics = this.picsCollection.slice(this.firstPicPerPageIndex, this.lastPicImageIndex);
+      this.selectedPage = document.getElementById("pageList");
+    });
   }
 
-  cenas(pic : Object) {
-    console.log(pic["id"]);
+  nextPage() {
+
+    if (this.currentPage >= this.numOfPages) return;
+
+    this.currentPage++;
+
+    this.changePage();
   }
 
+  previousPage() {
+
+    if (this.currentPage <= 1) return;
+
+    this.currentPage--;
+
+    this.changePage();
+  }
+
+  changePage() {
+
+    this.firstPicPerPageIndex = (this.currentPage - 1) * this.picsPerPage;
+
+    this.lastPicImageIndex = this.firstPicPerPageIndex + (this.picsPerPage - 1);
+
+    this.currentPics = this.picsCollection.slice(this.firstPicPerPageIndex, this.lastPicImageIndex);
+
+    this.selectedPage["value"] = this.currentPage;
+  }
+
+  generatePageList() {
+
+    var pageNums: number[] = [];
+  
+    for(var i = 0; i < this.numOfPages; i++) {
+      pageNums.push(i + 1);
+    }
+
+    return pageNums;
+  }
+
+  changePageManually(){
+
+    if (this.currentPage == this.selectedPage["value"]) return;
+
+    this.currentPage = this.selectedPage["value"];
+
+    this.changePage();
+  }
 }
